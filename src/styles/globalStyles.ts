2d5ccc684100287003324e359d650ab98ca9eca2
Frida Ulfves <frida.ulfves@bonniernews.se>
2024-01-16 20:51:46 +0100

    updating global styles

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,13 +1,36 @@
-// globalStyles.js
-import { createGlobalStyle } from 'styled-components';
- 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    background: teal;
-    font-family: Open-Sans, Helvetica, Sans-Serif;
-  }
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    
+    * {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+        font-weight: 400;
+        font-size: 62.5%;
+        height: 100vh;
+        color: white;
+        background-color: #1e213a;
+    }
+    #root {
+        display: flex;
+        justify-content: center;
+    }
+    h1 {
+        margin: 1.5rem;
+        font-size: 3rem;
+        letter-spacing: 0.2rem;
+        line-height: 1.1;
+    }
+    h3 {
+        margin: 0.5rem;
+        font-size: 1.2rem;
+    }
 `;
- 
-export default GlobalStyle;
+
+export default GlobalStyles;
