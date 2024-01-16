1287598b6a836dba4dfc9e2ed767f879dacf5882
Frida Ulfves <frida.ulfves@bonniernews.se>
2024-01-16 22:30:11 +0100

    updating home with navigation to the other pages and card component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,29 @@
+import { Link, Outlet } from "react-router-dom";
+import styled from "styled-components";
+import Card from "../components/card";
+
+const Pages = styled.button`
+  color: #363535;
+  margin-top: 5%;
+  font-size: 1.2rem;
+  padding: 0.5rem 1rem;
+  margin: 0.5rem;
+`;
+
 const Home = () => {
     return (
-      <div>
-        <h1>HOME</h1>
-      </div>
+      <>
+        <Card> <h1>This is the card</h1>        
+		<Link to="/details">
+          <Pages>Details</Pages>
+        </Link>
+        <Link to="/Idontknowyet">
+          <Pages>I don't know yet</Pages>
+        </Link>
+		</Card>
+
+        <Outlet />
+      </>
     );
   };
 
