7d9078d087d1447b05059443151597e2b8ab77cc
Frida Ulfves <frida.ulfves@bonniernews.se>
2024-01-17 19:55:26 +0100

    rendering data from context

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,10 +1,21 @@
+import { useContext } from "react";
+import Card from "../components/card";
+import { WeatherDataContext } from "../context/WeatherDataContext";
 
 const Details = () => {
-    return (
-      <div>
-        <h1>DETAILS</h1>
-      </div>
-    );
-  };
+  const context = useContext(WeatherDataContext);
 
-export default Details;  
+  if (!context || !context.weatherData) {
+    return <div>Ingen väderinformation tillgänglig</div>;
+  }
+
+  const { weatherData } = context;
+
+  return (
+    <Card>
+      <h1>{weatherData.weatherSymbol}</h1>
+    </Card>
+  );
+};
+
+export default Details;
