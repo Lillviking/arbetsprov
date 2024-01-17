89f21a419a1c6f59ebaf9caafeefed6c25eb5c60
Frida Ulfves <frida.ulfves@bonniernews.se>
2024-01-17 19:46:49 +0100

    updating global styles

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -3,24 +3,26 @@ import styled from "styled-components";
 
 // styled component
 const StyledCard = styled.div`
-    background-color: #fff;
-    border-radius: 30px;
-    box-shadow: 0 1px 3px rgba(0,0,0,0.2);
-    margin: 1rem;
-    padding: 1rem;
-    width: 50%;
-    height: 70%;
-    color: #363535;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  background-color: #fff;
+  border-radius: 30px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+  margin: 1rem;
+  padding: 1rem;
+  width: 30%;
+  height: 70%;
+  color: #363535;
 `;
 
 interface ICard {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 const Card: React.FC<ICard> = ({ children }) => {
-    return (
-        <StyledCard>{children}</StyledCard>
-    );
-  };
+  return <StyledCard>{children}</StyledCard>;
+};
 
-export default Card;  
+export default Card;
