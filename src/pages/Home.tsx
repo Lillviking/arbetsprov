463751857dc30dd0cc3abcb54804237ae226ab7d
Frida Ulfves <frida.ulfves@bonniernews.se>
2024-01-18 09:42:22 +0100

    cleaning up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,7 +19,6 @@ interface Parameter {
 }
 
 const Home = () => {
-  const [forecast, setForecast] = useState<any>(null);
   // lat long for Sundsvall
   const [latitude, setLatitude] = useState<number>(62.39);
   const [longitude, setLongitude] = useState<number>(17.3);
@@ -45,52 +44,33 @@ const Home = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // hämta data från SMHI
+        // fetch weather data from SMHI
         const endPoint = `https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/${longitude.toFixed(
           6
         )}/lat/${latitude.toFixed(6)}/data.json`;
         const response = await fetch(endPoint);
         const data = await response.json();
-
         const formattedData = formatWeatherData(data);
-        console.log(formattedData, "formatted data");
 
-        console.log(formattedData.t.value, "formatted data");
-        console.log(formattedData.Wsymb2.value, "formatted data symbol");
-
+        // set context data
         if (weatherContext) {
           const chosenWeatherParameters = {
-            temperatureValue: formattedData.t.value, // Ersätt med riktig data
-            weatherSymbol: formattedData.Wsymb2.value, // Ersätt med riktig data
+            temperatureValue: formattedData.t.value,
+            weatherSymbol: formattedData.Wsymb2.value,
           };
 
           weatherContext?.setWeatherData(chosenWeatherParameters);
         }
 
-        const firstDataobject = data.timeSeries[0].parameters.map(
-          (p: any) => p
-        );
-        console.log(firstDataobject, "first object");
-
-        console.log(data.timeSeries[0].parameters[0].values[0]);
-        const temperatureValue = data.timeSeries[0].parameters.find(
-          (p: any) => p.name === "t"
-        )?.values[0];
-        const weatherSymbol = data.timeSeries[0].parameters.find(
-          (p: any) => p.name === "Wsymb2"
-        )?.values[0];
-        setWeatherSymbol(weatherSymbol);
-        setTemperatureValue(temperatureValue);
-        console.log(temperatureValue);
-        console.log(weatherSymbol);
-        setForecast(data);
+        setWeatherSymbol(weatherContext?.weatherData?.weatherSymbol || 1);
+        setTemperatureValue(weatherContext?.weatherData?.temperatureValue || 0);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
-  }, [latitude, longitude]);
+  });
 
   return (
     <>
