e179ae6a128ab7d17f1ad9c8b2a09ced1798a50c
fridaUlfves <frida.ulfves@bonniernews.se>
2024-01-19 09:45:20 +0100

    creating table for weather data

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -5,6 +5,9 @@ import { WeatherDataContext } from "../context/WeatherDataContext";
 const Details = () => {
   const context = useContext(WeatherDataContext);
 
+  console.log("context", context);
+  
+
   if (!context || !context.weatherData) {
     return <div>Ingen väderinformation tillgänglig</div>;
   }
@@ -13,9 +16,40 @@ const Details = () => {
 
   return (
     <Card>
-      <h1>{weatherData.weatherSymbol}</h1>
+    <div>
+      <h1>Vädret i detalj</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>Temperatur:</td>
+            <td>{weatherData?.temperature.value}</td>
+            <td>{weatherData?.temperature.unit}</td>
+          </tr>
+          <tr>
+            <td>Vindhastighet:</td>
+            <td>{weatherData?.windSpeed.value}</td>
+            <td>{weatherData?.windSpeed.unit}</td>
+          </tr>
+          <tr>
+            <td>Wind Direction:</td>
+            <td>{weatherData?.windDirection.value}</td>
+            <td>{weatherData?.windDirection.unit}</td>
+          </tr>
+          <tr>
+            <td>Relativ luftfuktighet:</td>
+            <td>{weatherData?.relativeHumidity.value}</td>
+            <td>{weatherData?.relativeHumidity.unit}</td>
+          </tr>
+          <tr>
+            <td>Regnkategori:</td>
+            <td>{weatherData?.precipitationCategory.value}</td>
+            <td>{weatherData?.precipitationCategory.unit}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
     </Card>
   );
 };
 
-export default Details;
+export default Details; 
