e6688181d7b667b09b66e04bb1119ea7f7ece524
fridaUlfves <frida.ulfves@bonniernews.se>
2024-01-20 13:08:29 +0100

    minor format changes

--- src/pages/ChangePlace.tsx
+++ src/pages/ChangePlace.tsx
@@ -4,14 +4,18 @@ import Map from "../components/map";
 import { GlobalButton } from "../styles/globalStyleComponents";
 
 const ChangePlace = () => {
-    return (
-      <div>
-        <Card><h1>Byt plats</h1>
-          <Map /> 
-          <Link to="/"><GlobalButton>&laquo; Tillbaka</GlobalButton>
-          </Link></Card>
-      </div>
-    );
-  };
 
-export default ChangePlace;  
+  return (
+    <div>
+      <Card>
+        <h1>Byt plats</h1>
+        <Map />
+        <Link to="/">
+          <GlobalButton>&laquo; Tillbaka</GlobalButton>
+        </Link>
+      </Card>
+    </div>
+  );
+};
+
+export default ChangePlace;
