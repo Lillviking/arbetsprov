0742851e1138311a05f98ed5de9866827185e370
Frida Ulfves <frida.ulfves@bonniernews.se>
2024-01-24 08:30:14 +0100

    remove unnecessary comments

--- src/context/CoordinateDataContext.tsx
+++ src/context/CoordinateDataContext.tsx
@@ -1,36 +1,37 @@
 import React, { ReactNode, createContext, useState } from "react";
 
 export type CoordinateDataType = {
-    latitude: number;
-    longitude: number;
-    name?: string;
+  latitude: number;
+  longitude: number;
+  name?: string;
 };
 
 export const CoordinateDataContext = createContext<{
-    coordinateData: CoordinateDataType;
-    setCoordinateData: React.Dispatch<React.SetStateAction<CoordinateDataType>>;
+  coordinateData: CoordinateDataType;
+  setCoordinateData: React.Dispatch<React.SetStateAction<CoordinateDataType>>;
 }>({
-    coordinateData: { latitude: 62.38, longitude: 17.30, name: "Sundsvall" }, // Startvärden här
-    setCoordinateData: () => { },
+  coordinateData: { latitude: 62.38, longitude: 17.3, name: "Sundsvall" },
+  setCoordinateData: () => {},
 });
 
 interface ICoordinateDataProvider {
-    children: ReactNode;
+  children: ReactNode;
 }
 
-export const CoordinateDataProvider: React.FC<ICoordinateDataProvider> = ({ children }) => {
-    const [coordinateData, setCoordinateData] = useState<CoordinateDataType>({
-        latitude: 62.38,
-        longitude: 17.30,
-        name: "Sundsvall",
-    });
+export const CoordinateDataProvider: React.FC<ICoordinateDataProvider> = ({
+  children,
+}) => {
+  const [coordinateData, setCoordinateData] = useState<CoordinateDataType>({
+    latitude: 62.38,
+    longitude: 17.3,
+    name: "Sundsvall",
+  });
 
-    const value = { coordinateData, setCoordinateData };
+  const value = { coordinateData, setCoordinateData };
 
-    return (
-        <CoordinateDataContext.Provider value={value}>
-            {children}
-        </CoordinateDataContext.Provider>
-    );
+  return (
+    <CoordinateDataContext.Provider value={value}>
+      {children}
+    </CoordinateDataContext.Provider>
+  );
 };
-
